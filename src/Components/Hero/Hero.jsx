d4cced5a9038c395c77d778d517fea50513d07b3
Manish Kumar <mmm.manish163@gmail.com>
2024-08-12 22:54:00 +0530

    fixing evtgh

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,6 +8,12 @@ import { MdOutlineFileDownload } from "react-icons/md";
 import { CiLocationArrow1 } from "react-icons/ci";
 import { FaGithub, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 
+const socialLinks = [
+  { href: "https://github.com/iamanishh", icon: <FaGithub /> },
+  { href: "https://linkedin.com/in/iamanishh", icon: <FaLinkedinIn /> },
+  { href: "https://twitter.com/iamanish__", icon: <FaTwitter /> },
+];
+
 const Hero = () => {
   return (
     <section id="hero">
@@ -16,13 +22,11 @@ const Hero = () => {
           <img src={profile} alt="Profile" width={110} />
           <p className="name">Hi, I'm Manish Kumar.</p>
           <h2>Behind-the-scenes architect</h2>
-
           <p className="desc">
             With nearly two years of experience as a Software Engineer, I have
             contributed in creating efficient backend systems designed for
             scalability and performance.
           </p>
-
           <div className="hero-btn">
             <a href="#contact">
               Contact &nbsp;
@@ -33,17 +37,12 @@ const Hero = () => {
               <MdOutlineFileDownload />
             </a>
           </div>
-
           <div className="hero-social">
-            <a href="">
-              <FaGithub />
-            </a>
-            <a href="">
-              <FaLinkedinIn />
-            </a>
-            <a href="">
-              <FaTwitter />
-            </a>
+            {socialLinks.map((social, index) => (
+              <a key={index} href={social.href} target="_blank">
+                {social.icon}
+              </a>
+            ))}
           </div>
         </div>
       </div>
