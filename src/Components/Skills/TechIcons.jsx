77c1611462a1b6afd9912f2ecd733044465f3e70
Manish Kumar <mmm.manish163@gmail.com>
2024-08-27 00:27:29 +0530

    fixing skills

--- src/Components/Skills/TechIcons.jsx
+++ src/Components/Skills/TechIcons.jsx
@@ -66,7 +66,7 @@ function TechIcons({ rowsToShow }) {
                 className="tech-icon"
                 style={{ "--icon-color": color }}
               >
-                <IconComponent size={22} />
+                <IconComponent size={20} />
                 <p>{name}</p>
               </div>
             ))}
